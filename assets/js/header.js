f6cc18bc088f62aaf6d37c135a313ff0a2f678a1
Gabriele Pedesini <125191834+gabrielepedesini@users.noreply.github.com>
2024-10-02 23:53:48 +0200

    Add files via upload

---
+++ assets/js/header.js
@@ -0,0 +1,36 @@
+// modify the links
+const navbarLinks = [
+    { href: "index.html", text: "home" },
+    { href: "notes.html", text: "appunti" },
+    { href: "projects.html", text: "progetti" },
+    { href: "contacts.html", text: "contatti" }
+];
+
+// render the navbar
+function renderNavbar() {
+    const navbarContainer = document.querySelector('.navbar');
+
+    let navbarHTML = `
+        <div class="container">
+            <ul>
+    `;
+
+    navbarLinks.forEach(link => {
+        navbarHTML += `
+            <li>
+                <a href="${link.href}">${link.text}</a>
+            </li>
+        `;
+    });
+
+    navbarHTML += `
+            </ul>
+        </div>
+    `;
+
+    navbarContainer.innerHTML = navbarHTML;
+}
+
+window.addEventListener("load", () => {
+    renderNavbar();
+})
